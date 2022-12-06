a7f163d6b446ef8c5b0097c53297729372ef61f3
Jeslin-C-John <111417450+Jeslin-C-John@users.noreply.github.com>
2022-12-06 19:07:51 +0530

    Add project files.

---
+++ Login2/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Login2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+
+namespace Login2.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DataModel instance)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-28UGTAO;Initial Catalog=user;Integrated Security=True");
+            string SqlQuery = "select name from userlist where name=@name and password=@password";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(SqlQuery, con);
+            cmd.Parameters.AddWithValue("@name", instance.Name);
+            cmd.Parameters.AddWithValue("@Password", instance.Password);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.Read())
+            {
+                Session["Name"] = instance.Name.ToString();
+                return RedirectToAction("Welcome");
+            }
+            con.Close();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Welcome(DataModel instance)
+        {
+            return View();
+        }
+    }
+}
---
+++ Login2/Controllers/SignupController.cs
@@ -0,0 +1,41 @@
+﻿using Login2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login2.Controllers
+{
+    public class SignupController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DataModel instance)
+        {
+            DataModel DataModelobj=new DataModel();
+            using (SqlConnection con = new SqlConnection("Data Source=DESKTOP-28UGTAO;Initial Catalog=user;Integrated Security=True"))
+            {
+                using (SqlCommand cmd = new SqlCommand("insertuser", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", instance.Name);
+                    cmd.Parameters.AddWithValue("@password", instance.Password);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+
+            }
+            return View();
+
+        }
+    }
+}
